03d4a259db6986c57826c340ffbb4f39fc903ff9
callMeJovi <111741690+callMeJovi@users.noreply.github.com>
2022-10-02 16:23:51 +0200

    Delete Footer.js

--- Footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-export default function Footer() {
-    return (
-        <footer>
-            <small>@ 2022 jojo development. All rights reserved</small>
-        </footer>
-    )
-}
